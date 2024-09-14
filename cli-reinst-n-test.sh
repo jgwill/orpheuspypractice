212bbe6d92e17022c59fb817915847be058ab9f0
JGWill <jgi@jgwill.com>
2024-09-14 15:05:55 -0400

    fix:oabc
    oabc is the new cli
    issue #2

--- cli-reinst-n-test.sh
+++ cli-reinst-n-test.sh
@@ -1,4 +1,5 @@
 . _reinstall.sh 
 say_hello_orpheuspypractice && echo "Say hello CLI works, its a good sign" || echo "Say hello CLI failed, something is wrong"
-jgabcli2 --help
+oabc --help && echo "Orpheus ABC CLI works, its a good sign" || echo "Orpheus ABC CLI failed, something is wrong"
+
 
