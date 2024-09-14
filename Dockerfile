8d22ee0f8f13b547f6a141fa4778a1c7e3555d4a
JGWill <jgi@jgwill.com>
2024-09-14 18:43:39 -0400

    chg:Enhancement install test
    issue #5

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
 FROM jgwill/ubuntu:py3.10
 #ubuntu:22.04
 
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
 WORKDIR /install
 COPY dist/* .
 RUN pip install --no-cache-dir *.whl
 
 WORKDIR /scripts
 COPY scripts/* .
+RUN chmod +x *.sh
+
+WORKDIR /samples
+COPY samples/*.abc .
+RUN chmod 777 *
+RUN chmod 777 .
 
 # Add user jgi
 RUN useradd -ms /bin/bash jgi
--- scripts/odep_install_dep_n_test_if_installed.sh
+++ scripts/odep_install_dep_n_test_if_installed.sh
@@ -1,6 +1,10 @@
-odep install_abc2midi
-odep install_imagemagick
-odep install_musescore
+
+echo "Installing dependencies"
+odep install_abc2midi &>/dev/null && echo "abc2midi installtion ran successfully" || echo "abc2midi installation failed"
+odep install_imagemagick&>/dev/null && echo "ImageMagick installation ran successfully" || echo "ImageMagick installation failed"
+odep install_musescore &>/dev/null && echo "Musescore installation ran successfully" || echo "Musescore installation failed"
+
+echo "--------------------------"
 
 if [ "$(odep is_musescore_installed)" == "True" ];then echo "Musescore dependencies are ok";fi
 if [ "$(odep is_imagemagick_installed)" == "True" ];then echo "Image convertions dependencies are ok";fi
