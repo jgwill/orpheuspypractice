d75cf555f0d214318bd9e420702ad6d452aa1db8
JGWill <jgi@jgwill.com>
2024-09-14 17:14:06 -0400

    fix:build

--- _build.sh
+++ _build.sh
@@ -4,6 +4,7 @@ python bump_version.py
 git commit setup.py -m "Bump version"
 version=$(python setup.py --version)
 git tag $version -m "Release $version"
-git push origin $version
+git push --tags &>/dev/null
+git push  &>/dev/null
 rm -rf dist;python setup.py sdist bdist_wheel;twine upload dist/*
 
