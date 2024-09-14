29d541a973b6abb5b1fd8b7ef49f08ed522909b5
JGWill <jgi@jgwill.com>
2024-09-14 15:03:28 -0400

    Initial commit for branch 'testing-env'

--- _build.sh
+++
@@ -1,8 +0,0 @@
-export PYTHONPATH=$(pwd)/src
-python bump_version.py
-git commit setup.py -m "Bump version"
-version=$(python setup.py --version)
-git tag $version -m "Release $version"
-git push origin $(version)
-rm -rf dist;python setup.py sdist bdist_wheel;twine upload dist/*
-
--- src/orpheuspypractice/__init__.py
+++
@@ -1,8 +0,0 @@
-from jgcmlib.jgabcli import main as jgabcli_main
-
-
-
-
-def say_hello():
-    print("Hello, World!")
-    
