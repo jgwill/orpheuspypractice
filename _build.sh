b0cfbf001bb14d9ea2a022686d71f9878dae2c4e
JGWill <jgi@jgwill.com>
2024-09-14 17:14:32 -0400

    fix:build

--- _build.sh
+++ _build.sh
@@ -3,7 +3,8 @@ pip install -U jgtcmlib &>/dev/null
 python bump_version.py
 git commit setup.py -m "Bump version"
 version=$(python setup.py --version)
-git tag $version -m "Release $version"
+git tag $version 
+#-m "Release $version"
 git push --tags &>/dev/null
 git push  &>/dev/null
 rm -rf dist;python setup.py sdist bdist_wheel;twine upload dist/*
