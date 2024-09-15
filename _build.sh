04adb3dc6b7848c7664b341002a9dcb325f1194e
JGWill <jgi@jgwill.com>
2024-09-14 20:29:45 -0400

    fix:push after dist

--- _build.sh
+++ _build.sh
@@ -5,9 +5,9 @@ git commit setup.py -m "Bump version"
 version=$(python setup.py --version)
 git tag $version 
 #-m "Release $version"
+rm -rf dist
+python setup.py sdist bdist_wheel && twine upload dist/*
 sleep 2
 git push --tags &>/dev/null
 git push  &>/dev/null
-rm -rf dist
-python setup.py sdist bdist_wheel && twine upload dist/*
 
