afee34a056c4d37d6940a271f44975051993d66b
JGWill <jgi@jgwill.com>
2024-09-20 18:56:44 -0400

    jghfmanager

--- _justrebuild.sh
+++ _justrebuild.sh
@@ -1,6 +1,9 @@
 rm -rf dist
 pip install -U jgcmlib &>/dev/null;pip install -U jgcmlib &>/dev/null
 pip freeze|grep jgcmlib
+#jghfmanager
+pip install -U jghfmanager &>/dev/null;pip install -U jghfmanager &>/dev/null
+pip freeze|grep jghfmanager
 sleep 1
 python setup.py sdist bdist_wheel
 
