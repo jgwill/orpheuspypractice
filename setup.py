f108ef7f286bba997cbb17020b1fe1ee31b88b13
JGWill <jgi@jgwill.com>
2024-10-27 14:58:27 -0400

    future requirements for langchain

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         'console_scripts': [
             "oabc = orpheuspypractice:jgabcli_main",
             "omid2score = orpheuspypractice:jgabcli_main_mid2score",
-            "say_hello_orpheuspypractice = orpheuspypractice:say_hello",
+            "osay_hello_orpheuspypractice = orpheuspypractice:say_hello",
+            "olca = orpheuspypractice:olca:main",
             "odep = orpheuspypractice.dependency_action:main",
             "ohfi = orpheuspypractice:jgthfcli_main",
             "wfohfi_then_oabc_foreach_json_files = orpheuspypractice:wfohfi_then_oabc_foreach_json_files",
---
+++ src/orpheuspypractice/olca.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import json
+
+#langchain
+from langchain.langchain import LangChain
+
