6a00ac1ff11cf633fc161b3b4dd4254d89092b2a
JGWill <jgi@jgwill.com>
2024-10-27 22:20:55 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='orpheuspypractice',
-    version='0.2.20',
+    version='0.2.21',
     author='JGWill',
     author_email='jgi@jgwill.com',
     description='Practice Package to Experiment with Orpheus\'s goals and its submodules',
