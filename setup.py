4a7a8fb368be25e38ca947e4c3e5fcbb6639e027
JGWill <jgi@jgwill.com>
2025-08-08 07:40:13 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='orpheuspypractice',
-    version='0.3.0',
+    version='0.3.1',
     author='JGWill',
     author_email='jgi@jgwill.com',
     description='AI-Powered Music Generation & Interactive Composition Toolkit',
